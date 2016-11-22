6cbb76a36257cfa920f39374b7feff32e2c5627f
robinlyang <robiny@gmail.com>
2016-11-21 19:45:55 -0700

    Project

--- NETTWO_Files2/Form1.Designer.cs
+++ NETTWO_Files2/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.readFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveRTF = new System.Windows.Forms.Button();
+            this.readRTF = new System.Windows.Forms.Button();
+            this.saveSequential = new System.Windows.Forms.Button();
+            this.readSequential = new System.Windows.Forms.Button();
+            this.sequentialCSV = new System.Windows.Forms.Button();
+            this.sequentialFormatted = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(13, 71);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 2;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(105, 71);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(75, 23);
+            this.read.TabIndex = 3;
+            this.read.Text = "Read";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(13, 148);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(75, 23);
+            this.saveFile.TabIndex = 4;
+            this.saveFile.Text = "Save File";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // readFile
+            // 
+            this.readFile.Location = new System.Drawing.Point(105, 148);
+            this.readFile.Name = "readFile";
+            this.readFile.Size = new System.Drawing.Size(106, 23);
+            this.readFile.TabIndex = 5;
+            this.readFile.Text = "Read File";
+            this.readFile.UseVisualStyleBackColor = true;
+            this.readFile.Click += new System.EventHandler(this.readFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(555, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(21, 23);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // saveRTF
+            // 
+            this.saveRTF.Location = new System.Drawing.Point(13, 206);
+            this.saveRTF.Name = "saveRTF";
+            this.saveRTF.Size = new System.Drawing.Size(100, 23);
+            this.saveRTF.TabIndex = 7;
+            this.saveRTF.Text = "Save RTF";
+            this.saveRTF.UseVisualStyleBackColor = true;
+            this.saveRTF.Click += new System.EventHandler(this.saveRTF_Click);
+            // 
+            // readRTF
+            // 
+            this.readRTF.Location = new System.Drawing.Point(147, 206);
+            this.readRTF.Name = "readRTF";
+            this.readRTF.Size = new System.Drawing.Size(224, 23);
+            this.readRTF.TabIndex = 8;
+            this.readRTF.Text = "Read RTF";
+            this.readRTF.UseVisualStyleBackColor = true;
+            this.readRTF.Click += new System.EventHandler(this.readRTF_Click);
+            // 
+            // saveSequential
+            // 
+            this.saveSequential.Location = new System.Drawing.Point(404, 70);
+            this.saveSequential.Name = "saveSequential";
+            this.saveSequential.Size = new System.Drawing.Size(90, 51);
+            this.saveSequential.TabIndex = 9;
+            this.saveSequential.Text = "Save Sequential";
+            this.saveSequential.UseVisualStyleBackColor = true;
+            this.saveSequential.Click += new System.EventHandler(this.saveSequential_Click);
+            // 
+            // readSequential
+            // 
+            this.readSequential.Location = new System.Drawing.Point(534, 71);
+            this.readSequential.Name = "readSequential";
+            this.readSequential.Size = new System.Drawing.Size(93, 50);
+            this.readSequential.TabIndex = 10;
+            this.readSequential.Text = "Read Sequential";
+            this.readSequential.UseVisualStyleBackColor = true;
+            this.readSequential.Click += new System.EventHandler(this.readSequential_Click);
+            // 
+            // sequentialCSV
+            // 
+            this.sequentialCSV.Location = new System.Drawing.Point(404, 148);
+            this.sequentialCSV.Name = "sequentialCSV";
+            this.sequentialCSV.Size = new System.Drawing.Size(90, 60);
+            this.sequentialCSV.TabIndex = 11;
+            this.sequentialCSV.Text = "Sequential CSV";
+            this.sequentialCSV.UseVisualStyleBackColor = true;
+            this.sequentialCSV.Click += new System.EventHandler(this.sequentialCSV_Click);
+            // 
+            // sequentialFormatted
+            // 
+            this.sequentialFormatted.Location = new System.Drawing.Point(534, 148);
+            this.sequentialFormatted.Name = "sequentialFormatted";
+            this.sequentialFormatted.Size = new System.Drawing.Size(93, 60);
+            this.sequentialFormatted.TabIndex = 12;
+            this.sequentialFormatted.Text = "Sequential Formatted";
+            this.sequentialFormatted.UseVisualStyleBackColor = true;
+            this.sequentialFormatted.Click += new System.EventHandler(this.sequentialFormatted_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 282);
+            this.Controls.Add(this.sequentialFormatted);
+            this.Controls.Add(this.sequentialCSV);
+            this.Controls.Add(this.readSequential);
+            this.Controls.Add(this.saveSequential);
+            this.Controls.Add(this.readRTF);
+            this.Controls.Add(this.saveRTF);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.readFile);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.read);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.Button readFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button saveRTF;
+        private System.Windows.Forms.Button readRTF;
+        private System.Windows.Forms.Button saveSequential;
+        private System.Windows.Forms.Button readSequential;
+        private System.Windows.Forms.Button sequentialCSV;
+        private System.Windows.Forms.Button sequentialFormatted;
     }
 }
 
--- NETTWO_Files2/Form1.cs
+++ NETTWO_Files2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NETTWO_Files2
 {
@@ -15,6 +16,98 @@ namespace NETTWO_Files2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {//save in Settings
+            data d = new NETTWO_Files2.data();
+            d.name = textBox1.Text;
+            d.phone = textBox2.Text;
+            d.Save();
+        }
+
+        private void read_Click(object sender, EventArgs e)
+        {//read in Settings
+            data d = new NETTWO_Files2.data();
+            textBox1.Text = d.name;
+            textBox2.Text = d.phone;
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {//save using a File Class
+            string[] stuff = { textBox1.Text, textBox2.Text };
+            File.WriteAllLines(@"C:\Users\ryang\Desktop\stuff.txt", stuff);
+        }
+
+        private void readFile_Click(object sender, EventArgs e)
+        {
+            string[] stuff = File.ReadAllLines(@"C:\Users\ryang\Desktop\stuff.dat");
+            textBox1.Text = stuff[0];
+            textBox2.Text = stuff[1];
+        }
+
+        private void saveRTF_Click(object sender, EventArgs e)
+        {//save RTF
+            richTextBox1.Text = textBox1.Text + "," + textBox2.Text;
+            richTextBox1.SaveFile(@"C:\Users\ryang\Desktop\rtf.dat");
+        }
+
+        private void readRTF_Click(object sender, EventArgs e)
+        {//read RTF
+            richTextBox1.LoadFile(@"C:\Users\ryang\Desktop\rtf.dat");
+            string[] data = richTextBox1.Text.Split(',');
+            textBox1.Text = data[0];
+            textBox2.Text = data[1];
+        }
+
+        private void saveSequential_Click(object sender, EventArgs e)
+        {//save Sequential
+            //before objects came along
+            //open "c:\data\stuff.dat" for output as #1
+            //write #1, x
+            //write #1, y
+            //close #1
+
+            //now with .NET
+            FileStream f = new FileStream(@"C:\Users\ryang\Desktop\seq.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter w = new StreamWriter(f);
+            w.WriteLine(textBox1.Text);
+            w.WriteLine(textBox2.Text);
+            w.Close();
+            f.Close();
+        }
+
+        private void readSequential_Click(object sender, EventArgs e)
+        {//read Sequential
+            FileStream f = new FileStream(@"C:\Users\ryang\Desktop\seq.dat", FileMode.Open, FileAccess.Read);
+            StreamReader r = new StreamReader(f);
+            textBox1.Text = r.ReadLine();
+            textBox2.Text = r.ReadLine();
+            r.Close();
+            f.Close();
+        }
+
+        private void sequentialCSV_Click(object sender, EventArgs e)
+        {//write many csv records & read back
+            string[] data = { "abc", "123", "xyz" };
+            string record = string.Join(",", data); //creates string ready for csv file
+            StreamWriter w = new StreamWriter(@"C:\Users\ryang\Desktop\demo.csv");  //one line StreamWriter destroys and recreates file
+            w.WriteLine(record);
+            w.WriteLine(record);
+            w.Close();
+            StreamReader r = new StreamReader(@"C:\Users\ryang\Desktop\demo.csv");
+            while((record = r.ReadLine()) != null)
+            {
+                //pull apart csv record
+                string[] stuff = record.Split(',');
+                //now use stuff[0], stuff[1], etc.
+            }
+            r.Close();
+        }
+
+        private void sequentialFormatted_Click(object sender, EventArgs e)
+        {//Sequential Formatted
+
         }
     }
 }
